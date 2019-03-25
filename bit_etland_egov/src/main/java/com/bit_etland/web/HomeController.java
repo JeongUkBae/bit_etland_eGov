aa8b4cdf52df2b43285d0897697389f810db35a6
1027 <1027@hb1006>
2019-03-25 20:55:49 +0900

    2019.03.25 20:55

--- bit_etland_egov/src/main/java/com/bit_etland/web/HomeController.java
+++ bit_etland_egov/src/main/java/com/bit_etland/web/HomeController.java
@@ -9,16 +9,14 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * Handles requests for the application home page.
  */
-@SessionAttributes("ctx")
 @Controller
 public class HomeController {
 	
@@ -27,17 +25,12 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@Autowired HttpSession session;
-	@Autowired HttpServletRequest request;
 	@RequestMapping("/")
 	public String home(Locale locale, Model model, HttpSession session, 
 			HttpServletRequest request) {
 		
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		
-		String ctx = request.getContextPath();
-		session.setAttribute("ctx", ctx);
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -45,6 +38,6 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home.jsp";
+		return "index";
 	}
 }
